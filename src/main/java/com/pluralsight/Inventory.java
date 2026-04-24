6a089eecebe37d3abc270672c14193444f87825e
killioin007-source <killioin.007@gmail.com>
2026-04-24 10:45:05 -0400

    added new code

---
+++ src/main/java/com/pluralsight/Inventory.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class Inventory {
+    private int id;
+    private String name;
+    private float price;
+
+    public Inventory(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,49 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HashMap <Integer, Inventory> products = new HashMap<Integer, Inventory>();
+        try{
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/inventory.csv"));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("\\|");
+
+            int id = Integer.parseInt(parts[0]);
+            String name = parts[1];
+            Float price = Float.parseFloat(parts[2]);
+
+            Inventory product = new Inventory(id, name, price);
+            products.put(id, product);
+        }
+        reader.close();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+        boolean searchAgain = true;
+
+        while (searchAgain) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What product would you like to search? (Enter X to quit.)");
+            int userInput = Integer.parseInt(scanner.nextLine());
+
+            switch (userInput) {
+                case('X'):
+                    searchAgain = false;
+                    break;
+                default:
+                    Inventory print = products.get(userInput);
+                    System.out.println(print.toString());
+            }
         }
     }
 }
